a71b9707a659be56fcc072e390d6d2ff3687ce3b
Ryan Shu <rrs6624@g.rit.edu>
2026-04-10 23:42:55 -0400

    Summary
    Player now Moves
    
    プレイヤーが移動します

--- CatPunch/Assets/Ryan/Scripts/PlayerMovement.cs
+++ CatPunch/Assets/Ryan/Scripts/PlayerMovement.cs
@@ -1,30 +1,52 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
-    private Rigidbody rb;
-    public int forceMagnitude = 1;
-    private float speed;
+
     private bool grounded = true;
+    private CharacterController characterController;
+    private Vector2 moveInput;
+    private Vector3 velocity;
+
+    //movement
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpHeight = 2f;
+    [SerializeField] private float gravity = -9.8f;
+
+    //ground check
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private float checkRadius = 0.2f;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        characterController = GetComponent<CharacterController>();
     }
 
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+        Debug.Log($"Move Input: {moveInput}");
+        Debug.Log("Received input");
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        Debug.Log($"Jumping: {context.performed} - Is Grounded: {characterController.isGrounded}");
+        if (context.performed && characterController.isGrounded)
+        {
+            Debug.Log("Jump initiated");
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.AddForce(new Vector3(0, forceMagnitude, 0), ForceMode.VelocityChange);
-        while (grounded)
-        {
-            rb.linearDamping = 5;
-        }
-        while (!grounded)
-        {
-            rb.linearDamping = 0.5f;
-        }
+        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
+        characterController.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        characterController.Move(velocity * Time.deltaTime);
     }
 }
