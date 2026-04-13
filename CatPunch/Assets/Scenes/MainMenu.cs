66948f1b4060ff01215d7cf75bc376767a113c67
Amy Lee <iamseoyoung05@gmail.com>
2026-04-12 22:14:15 -0400

    Starting Menu

---
+++ CatPunch/Assets/Scenes/MainMenu.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    // Reference to panels
+    public Image controlPanel;
+    public Image UIPanel;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        controlPanel.gameObject.SetActive(false);
+        UIPanel.gameObject.SetActive(false);
+    }
+
+    private void OnDisable()
+    {
+    }
+
+    public void ClickStartButton()
+    {
+        SceneManager.LoadScene("JayEnvironment");
+    }
+
+    public void ClickControlButton()
+    {
+        controlPanel.gameObject.SetActive(true);
+    }
+
+    public void ClickCreditButton()
+    {
+        UIPanel.gameObject.SetActive(true);
+    }
+
+    public void Cancel()
+    {
+        Debug.Log("Panels Disabled!");
+        controlPanel.gameObject.SetActive(false);
+        UIPanel.gameObject.SetActive(false);
+    }
+}
